e14b310cf97b008762501f2a994e1d0ea9110955
Sharmaketan <ketan.sharma@iensure.in>
2024-05-31 17:39:34 +0530

    configuring the view engine, express template, public url, route handler

--- src/server.js
+++ src/server.js
@@ -4,4 +4,11 @@ const app = express()
 
 console.log("HERLLLO")
 
+app.set("view engine", "pug")
+
+app.set("views", __dirname + "/views")
+app.use("/public", express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => res.render("home"))
+
 app.listen(4000)
