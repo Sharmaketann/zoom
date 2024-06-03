9291732b8858d893e3008b3f3b7f4876b881c2e3
Sharmaketan <ketan.sharma@iensure.in>
2024-06-03 17:46:25 +0530

    Setting up WebSocket

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express"
+import http from "http"
+import WebSocket from "ws"
 
 const app = express()
 
@@ -8,7 +10,18 @@ app.set("view engine", "pug")
 
 app.set("views", __dirname + "/views")
 app.use("/public", express.static(__dirname + "/public"))
-
 app.get("/", (req, res) => res.render("home"))
 
-app.listen(4000)
+const handleListen = () => console.log(`Listening on XXXXXXXXXXXXXXXXXXXXX`)
+
+const server = http.createServer(app)
+
+const wss = new WebSocket.Server({ server })
+
+function handleConnection(socket) {
+  console.log(socket)
+}
+
+wss.on("connection", handleConnection)
+
+server.listen(3000, handleListen)
