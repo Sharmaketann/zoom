ccd82d1d992b2d71c0931fb493d1059f3d186352
Sharmaketan <ketan.sharma@iensure.in>
2024-06-05 11:18:05 +0530

    sending and receiving message on multiple browser

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,45 @@
+const messageList = document.querySelector("ul")
+const messageForm = document.querySelector("form")
 const socket = new WebSocket(`ws://${window.location.host}`)
 
-socket.addEventListener("open", () => {
-  console.log("Connected to Server")
-})
+function handleOpen() {
+  console.log("Connected to Server ✅")
+}
+
+socket.addEventListener("open", handleOpen)
 
 socket.addEventListener("message", (message) => {
-  console.log("Just got this: ", message.data, "from the server")
+  if (message.data instanceof Blob) {
+    const reader = new FileReader()
+    reader.onload = (event) => {
+      const text = event.target.result
+      console.log("New message: ", text)
+
+      // Optionally, add the message to the message list
+      const li = document.createElement("li")
+      li.innerText = text
+      messageList.appendChild(li)
+    }
+    reader.readAsText(message.data)
+  } else {
+    console.log("New message: ", message.data)
+
+    // Optionally, add the message to the message list
+    const li = document.createElement("li")
+    li.innerText = message.data
+    messageList.appendChild(li)
+  }
 })
 
 socket.addEventListener("close", () => {
-  console.log("Disconnected from Server")
+  console.log("Disconnected from Server ❌")
 })
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const input = messageForm.querySelector("input")
+  socket.send(input.value)
+  input.value = ""
+}
+
+messageForm.addEventListener("submit", handleSubmit)
