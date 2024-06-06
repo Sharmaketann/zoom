b7a5044f07221ea440b8f8303ab28999b75e50b6
Sharmaketan <ketan.sharma@iensure.in>
2024-06-06 18:25:43 +0530

    Room left message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -35,3 +35,7 @@ form.addEventListener("submit", handleRoomSubmit)
 socket.on("welcome", () => {
   addMessage("Someone joined!")
 })
+
+socket.on("bye", () => {
+  addMessage("Someone left 😢")
+})
--- src/server.js
+++ src/server.js
@@ -26,6 +26,9 @@ wsServer.on("connection", (socket) => {
     done()
     socket.to(roomName).emit("welcome")
   })
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"))
+  })
 })
 
 // const wss = new WebSocket.Server({ server })
