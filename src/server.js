c3f50e1caee3e7e758b860f8f196ced874f19d25
Sharmaketan <ketan.sharma@iensure.in>
2024-06-06 10:47:14 +0530

    installing Socket.io

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http"
 import WebSocket from "ws"
 import express from "express"
+import SocketIO from "socket.io"
 
 const app = express()
 
@@ -12,35 +13,41 @@ app.get("/*", (_, res) => res.redirect("/"))
 
 const handleListen = () => console.log(`Listening on http://192.168.0.178:3000`)
 
-const server = http.createServer(app)
-const wss = new WebSocket.Server({ server })
+const httpServer = http.createServer(app)
+const wsServer = SocketIO(httpServer)
 
-function onSocketClose() {
-  console.log("Disconnected from the Browser ❌")
-}
-
-const sockets = []
-
-wss.on("connection", (socket) => {
-  sockets.push(socket)
-  socket["nickname"] = "Anon"
-  console.log("Connected to Browser ✅")
-  socket.on("close", onSocketClose)
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg)
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${`${socket.nickname}: `}${message.payload}`)
-        )
-
-      case "nickname":
-        socket["nickname"] = message.payload
-
-      default:
-        break
-    }
-  })
+wsServer.on("connection", (socket) => {
+  console.log("✅", socket)
 })
 
-server.listen(3000, "192.168.0.178", handleListen)
+// const wss = new WebSocket.Server({ server })
+
+// function onSocketClose() {
+//   console.log("Disconnected from the Browser ❌")
+// }
+
+// const sockets = []
+
+// wss.on("connection", (socket) => {
+//   sockets.push(socket)
+//   socket["nickname"] = "Anon"
+//   console.log("Connected to Browser ✅")
+//   socket.on("close", onSocketClose)
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg)
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${`${socket.nickname}: `}${message.payload}`)
+//         )
+
+//       case "nickname":
+//         socket["nickname"] = message.payload
+
+//       default:
+//         break
+//     }
+//   })
+// })
+
+httpServer.listen(3000, "192.168.0.178", handleListen)
